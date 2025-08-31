02f18971b56c86e7126afe133d5e1c42249a1cf4
Mashudu Molema <mrkaymp@gmail.com>
2025-08-31 11:29:46 +0200

    Updated get_redis_cache_metrics()

--- properties/utils.py
+++ properties/utils.py
@@ -22,14 +22,16 @@ def get_redis_cache_metrics():
     Returns a dictionary with metrics.
     """
     try:
-        # Connect to Redis
-        redis_conn = get_redis_connection("default")  # 'default' cache alias
+        redis_conn = get_redis_connection("default")
         info = redis_conn.info("stats")
 
         hits = info.get("keyspace_hits", 0)
         misses = info.get("keyspace_misses", 0)
-        total = hits + misses
-        hit_ratio = hits / total if total > 0 else 0.0
+
+        try:
+            hit_ratio = hits / (hits + misses)
+        except ZeroDivisionError:
+            hit_ratio = 0.0
 
         metrics = {
             "keyspace_hits": hits,
